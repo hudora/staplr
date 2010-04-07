d78642428a81aa09976eecb0cca283c8780c22ed
Axel Schlueter <axel@pqrs.de>
2010-04-07 13:02:42 +0200

    Ueberarbeitung

--- stapler/urls.py
+++ stapler/urls.py
@@ -1,21 +1,19 @@
-# encoding: utf-8
 
 from django.conf.urls.defaults import patterns
 from django.views.generic.simple import direct_to_template
 from stapler.decorators import login_required
-
 from stapler.models import Staplerjob
 
 urlpatterns = patterns('stapler.views',
-    (r'home/$', 'home'),
-    (r'holen/$',  'holen'),
-    (r'zurueckmelden/$', 'zurueckmelden'),
+    (r'^login', 'do_login'),
+    (r'^logout', 'do_logout'),
+    (r'^is_logged_in', 'is_logged_in'),
+    (r'^fetch_movement', 'fetch_movement'),
+    (r'^commit_movement/(?P<oid>[\w\d]+)', 'commit_or_cancel_movement', {'what': 'commit'}),
+    (r'^cancel_movement/(?P<oid>[\w\d]+)', 'commit_or_cancel_movement', {'what': 'cancel'}),
+    (r'^$', 'index'),
 )
 
-urlpatterns += patterns('',
-    (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'stapler/stapler.html'}),
-    (r'job/(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', 
-                                {'queryset': Staplerjob.objects.all()}),
-    (r'job/$', 'django.views.generic.list_detail.object_list', {'queryset': Staplerjob.objects.all()}),
+urlpatterns += patterns('django.views',
+    (r'^files/(?P<path>.*)$', 'static.serve', {'document_root': 'stapler/files'}),
 )
-
--- stapler/views.py
+++ stapler/views.py
@@ -1,90 +1,89 @@
-# encoding: utf-8
-""""
-views.py
 
-Created by Christian Klein on 2010-03-26.
-Copyright (c) 2010 HUDORA GmbH. All rights reserved.
-"""
-
+import random
+import json
+import time
 import datetime
+
 from django.http import HttpResponse, Http404
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404
 from django.views.decorators.http import require_POST
-
-from stapler.models import Staplerjob, make_job
-from stapler.decorators import login_required
-from hudjango.decorators import ajax_request
+from django.contrib.auth import authenticate, login, logout
 #from myplfrontend.kernelapi import Kerneladapter
-from cs.zwitscher import zwitscher
-import pprint
+
+from models import Staplerjob, make_job
+from decorators import login_required
 
 
-@login_required
-@ajax_request
-def home(request):
-    """View zur Feststellung, ob User eingeloggt ist."""
-    return {'status': 'OK', 'username': request.user.username}
+def index(request):
+    return render_to_response('stapler/application.html',{})
 
+def is_logged_in(request):
+    is_logged_in = request.user.is_authenticated()
+    job_count = Staplerjob.objects.filter(status='open', user=request.user).count()
+    current_movement = job_count > 0
+    return _render_to_json({'login':is_logged_in,
+                            'current_movement': current_movement})
 
-@login_required
 @require_POST
-def holen(request):
-    """Staplerjob anfordern"""
+def do_login(request):
+    username = 'schluete' # request.POST['username']
+    password = 'pyrrha' #request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return _render_to_json({'status':'OK'})
+    return _render_to_json({'status':'FAIL'})
+
+@require_POST
+def do_logout(request):
+    logout(request)
+    return _render_to_json({'status':'OK'})
+
+@login_required
+def has_current_movement(request):
+    try:
+        return _render_to_json({'movement':True})
+    except:
+        return _render_to_json({'movement':False})
+
+
+@require_POST
+@login_required
+def fetch_movement(request):
     try:
         job = Staplerjob.objects.get(status='open', user=request.user)
     except Staplerjob.DoesNotExist:
-        
-        movement = {'artnr': '10225',
-         'attr': 'test',
-         'created_at': '2010-03-29T09:39:00.855806Z',
-         'from_location': '110303',
-         'menge': 11,
-         'mui': '340059981002670930',
-         'mypl_notify_requesttracker': True,
-         'oid': 'mb09191995',
-         'reason': 'requesttracker',
-         'status': 'open',
-         'to_location': '032201'}
-        
         # movement = Kerneladapter().get_next_movement(attr='%s via myPL Stapler' % request.user.username)
-        
-        pprint.pprint(movement)
-        if not movement:
-            raise Http404("No movement available.")
-        #zwitscher("Movement %s via Stapler Terminal erzeugt" % movement['oid'], username="stapler")
+        id = str(random.randint(10,90))
+        movement = {'artnr': '10225'+id,
+                    'attr': 'test',
+                    'created_at': '2010-03-29T09:39:00.855806Z',
+                    'from_location': '110303',
+                    'menge': 11,
+                    'mui': '340059981002670930',
+                    'mypl_notify_requesttracker': True,
+                    'oid': 'mb091919'+id,
+                    'reason': 'requesttracker',
+                    'status': 'open',
+                    'to_location': '032201'}
         job = make_job(request.user, movement)
-    return HttpResponse(job.serialized_movement, mimetype='text/plain')
+    return HttpResponse(job.serialized_movement, mimetype='application/json')
 
-
-@login_required
 @require_POST
-@ajax_request
-def zurueckmelden(request):
-    """
-    Staplerjob zurückmelden
-    
-    Wenn der Auftrag nicht erfolgreich ausgeführt werden konnte,
-    ist das Feld 'storno' im POST-Dictionary gesetzt.
-    """
-
-    movement_id = request.POST.get('movement_id', '')
-    print "movement_id:", movement_id
-    print "100000000000"
-    job = get_object_or_404(Staplerjob, movement_id=movement_id, user=request.user, status='open')
-    print "200000000000"
-    if 'storno' in request.POST:
-        print "STORNO!"
-        # zwitscher("Staplerauftrag %s wurde storniert", username="stapler")
-        # automatisches Storno?
-        # Kerneladapter().movement_stornieren(movement_id, request.user.name, "Storno von Stapler")
+@login_required
+def commit_or_cancel_movement(request, what, oid):
+    job = get_object_or_404(Staplerjob, movement_id=oid, user=request.user, status='open')
+    if what == 'storno':
+        # Kerneladapter().movement_stornieren(oid, request.user.name, 'Storno via myPL Stapler')
         job.status = 'canceled'
     else:
-        print "COMMIT!"
-        # Kerneladapter().commit_movement(movement_id)
+        # Kerneladapter().commit_movement(oid)
         job.status = 'closed'
-    print "300000000000"
     job.closed_at = datetime.datetime.now()
-    print "400000000000"
     job.save()
-    print "500000000000"
-    return {'status': 'OK'}
+    return _render_to_json({ 'status': 'OK' })
+
+def _render_to_json(data):
+    json_data = json.dumps(data)
+    return HttpResponse(json_data, mimetype='application/json')
